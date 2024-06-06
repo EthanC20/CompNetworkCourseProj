869d7653e11fddd2d6ee8e2cd60b48661e541cf3
EthanC <943192544@qq.com>
2024-06-06 17:09:13 +0800

    ICMP passed!

--- src/icmp.c
+++ src/icmp.c
@@ -11,22 +11,46 @@
 static void icmp_resp(buf_t *req_buf, uint8_t *src_ip)
 {
     // TO-DO
+    // 初始化txbuf,封装icmp响应包
+    buf_init(&txbuf, req_buf->len);
+    memcpy(txbuf.data, req_buf->data, req_buf->len);
+    icmp_hdr_t *hdr = (icmp_hdr_t *)txbuf.data;
+    icmp_hdr_t *req_hdr = (icmp_hdr_t *)req_buf->data;
+    hdr->type = ICMP_TYPE_ECHO_REPLY;
+    hdr->code = 0;
+    hdr->id16 = req_hdr->id16;
+    hdr->seq16 = req_hdr->seq16;
+
+    // 填写校验和
+    hdr->checksum16 = 0;
+    hdr->checksum16 = checksum16((uint16_t *)txbuf.data, txbuf.len);
+
+    // 发送icmp响应包
+    ip_out(&txbuf, src_ip, NET_PROTOCOL_ICMP);
 }
 
 /**
  * @brief 处理一个收到的数据包
- * 
+ *
  * @param buf 要处理的数据包
  * @param src_ip 源ip地址
  */
 void icmp_in(buf_t *buf, uint8_t *src_ip)
 {
     // TO-DO
+    // 检查icmp报文长度是否正确
+    if (buf->len < sizeof(icmp_hdr_t)) return;
+
+    // 检查ICMP是否为回显请求
+    icmp_hdr_t *hdr = (icmp_hdr_t *)buf->data;
+
+    // 发送icmp响应
+    if (hdr->type == ICMP_TYPE_ECHO_REQUEST) icmp_resp(buf, src_ip);
 }
 
 /**
  * @brief 发送icmp不可达
- * 
+ *
  * @param recv_buf 收到的ip数据包
  * @param src_ip 源ip地址
  * @param code icmp code，协议不可达或端口不可达
@@ -34,6 +58,30 @@ void icmp_in(buf_t *buf, uint8_t *src_ip)
 void icmp_unreachable(buf_t *recv_buf, uint8_t *src_ip, icmp_code_t code)
 {
     // TO-DO
+    // 初始化txbuf,封装icmp包
+    int total_size = sizeof(icmp_hdr_t) + sizeof(ip_hdr_t) + 8;
+    buf_init(&txbuf, total_size);
+    icmp_hdr_t *icmp_hdr = (icmp_hdr_t *)txbuf.data;
+    icmp_hdr->type = ICMP_TYPE_UNREACH;
+    icmp_hdr->code = code;
+    icmp_hdr->id16 = 0;
+    icmp_hdr->seq16 = 0;
+
+    // 填写ICMP数据,IP数据报首部,数据字段和校验和
+    // 填写首部
+    ip_hdr_t *ip_hdr = (ip_hdr_t *)(icmp_hdr + 1);
+    ip_hdr_t *recv_ip_hdr = (ip_hdr_t *)recv_buf->data;
+    memcpy(ip_hdr, recv_ip_hdr, sizeof(ip_hdr_t));
+    // 填写数据
+    uint8_t *data = (uint8_t *)(ip_hdr + 1);
+    uint8_t *recv_ip_data = (uint8_t *)(recv_ip_hdr + 1);
+    memcpy(data, recv_ip_data, 8);
+    // 填写校验和
+    icmp_hdr->checksum16 = 0;
+    icmp_hdr->checksum16 = checksum16((uint16_t *)txbuf.data, total_size);
+
+    // 发送icmp包
+    ip_out(&txbuf, src_ip, NET_PROTOCOL_ICMP);
 }
 
 /**
